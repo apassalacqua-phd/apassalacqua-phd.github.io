9904ec8bb258424551403d9bdb05009429c22076
XaFaK-01 <sanzarfarooq01@gmail.com>
2024-08-26 01:46:28 +0500

    requested ui changes

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -11,49 +11,69 @@ const Timeline = ({ title, iconSrc, data, id }) => {
       <SectionTitleAndIcon iconSrc={iconSrc} title={title} />
 
       <div className={styles.timeline}>
-        {data.length > 0 && data.map((item, index) => (
-          <div key={index} className={styles.timelineItem}>
-            <div className={styles.timelineDate}>
-              {item.startYear}
-              {item.isPresent
-                ? " - Present"
-                : item.endYear
-                ? ` - ${item.endYear}`
-                : ""}
-            </div>
-            <div className={styles.timelineContent}>
-              <div className={styles.entityNameAndLocationContainerDesktop}>
-                <h4 className={styles.timelineHeading}>{item.entityName}</h4>
-                <h4 className={styles.timelineHeading}>
-                  <MdOutlineLocationOn className={styles.locationIcon} />
-                  {item.location}
-                </h4>
+        {data.length > 0 &&
+          data.map((item, index) => (
+            <div key={index} className={styles.timelineItem}>
+              <div className={styles.timelineDate}>
+                {item.startYear}
+                {item.isPresent
+                  ? " - Present"
+                  : item.endYear
+                  ? ` - ${item.endYear}`
+                  : ""}
               </div>
+              <div className={styles.timelineContent}>
+                {/* company name and location for desktop only */}
+                <div className={styles.entityNameAndLocationContainerDesktop}>
+                  <h4 className={styles.timelineHeading}>{item.entityName}</h4>
+                  <h4 className={styles.timelineHeading}>
+                    <MdOutlineLocationOn className={styles.locationIcon} />
+                    {item.location}
+                  </h4>
+                </div>
 
-              <div className={styles.entityNameAndLocationContainerMobile}>
-                <h4 className={styles.timelineHeading}>
-                  {`${item.entityName} - ${item.location}`}
-                </h4>
-              </div>
-              {item.position && (
-                <h5 className={styles.timelinePosition}>{item.position}</h5>
-              )}
+                {/* company name and location for mobile only */}
+                <div className={styles.entityNameAndLocationContainerMobile}>
+                  <h4 className={styles.timelineHeading}>
+                    {`${item.entityName} - ${item.location}`}
+                  </h4>
+                </div>
 
-              <ul>
-                {item?.data?.length>0 &&  item.data.map((point, i) => (
-                  <li
+                {/* job position */}
+                {item.position && (
+                  <h5
                     style={{
                       marginBottom: index + 1 === data.length ? 0 : "16px",
                     }}
-                    key={i}
+                    className={styles.timelinePosition}
                   >
-                    {point}
-                  </li>
-                ))}
-              </ul>
+                    {item.position}
+                  </h5>
+                )}
+
+                {/* data bullet points */}
+                {item?.data?.length > 0 && (
+                  <ul>
+                    {item.data.map((point, i) => (
+                      <li
+                        style={{
+                          marginBottom:
+                            index + 1 === data.length
+                              ? 0
+                              : i + 1 < item.data.length
+                              ? "0px"
+                              : "18px",
+                        }}
+                        key={i}
+                      >
+                        {point}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   )
--- src/constants/WorkExperienceTimelineData.js
+++ src/constants/WorkExperienceTimelineData.js
@@ -6,8 +6,8 @@ const workExperienceTimelineData = [
     location: "WASHINGTON, DC, US",
     position: "Financial Economist - Vice President & Research Lead",
     // data: [
-      // "Lead and prioritize impactful research projects related to small businesses and local economies, managing and triaging inbound requests for analysis to ensure timely completion and relevance.",
-      // "Conduct original analyses of large datasets using expert coding practices and big data programming languages on a public cloud platform, producing comprehensive statistical and visual analytics.",
+    // "Lead and prioritize impactful research projects related to small businesses and local economies, managing and triaging inbound requests for analysis to ensure timely completion and relevance.",
+    // "Conduct original analyses of large datasets using expert coding practices and big data programming languages on a public cloud platform, producing comprehensive statistical and visual analytics.",
     // ],
   },
 
@@ -19,8 +19,8 @@ const workExperienceTimelineData = [
     position:
       "Visiting Research Fellow at the AI, Analytics, and the Future of Work Initiative",
     // data: [
-      // "Lead and prioritize impactful research projects related to small businesses and local economies, managing and triaging inbound requests for analysis to ensure timely completion and relevance.",
-      // "Conduct original analyses of large datasets using expert coding practices and big data programming languages on a public cloud platform, producing comprehensive statistical and visual analytics.",
+    // "Lead and prioritize impactful research projects related to small businesses and local economies, managing and triaging inbound requests for analysis to ensure timely completion and relevance.",
+    // "Conduct original analyses of large datasets using expert coding practices and big data programming languages on a public cloud platform, producing comprehensive statistical and visual analytics.",
     // ],
   },
 
@@ -49,10 +49,10 @@ const workExperienceTimelineData = [
     location: "WASHINGTON, DC, US",
     position: "Economist",
     // data: [
-      // "Researched, designed, and implemented models to forecast banks’ potential losses during different hypothetical scenarios in the Dodd-Frank Act Stress Tests (DFAST).",
-      // "Monitored, analyzed, and quantified the economic impact of the COVID-19 pandemic and other events in critical markets such as the leveraged lending market.",
-      // "Investigated and wrote reports on specific issues requested by the Governors.",
-      // "Generated high-quality and novel academic research in macroeconomics, empirical banking, household finance, and financial institutions using a large confidential dataset",
+    // "Researched, designed, and implemented models to forecast banks’ potential losses during different hypothetical scenarios in the Dodd-Frank Act Stress Tests (DFAST).",
+    // "Monitored, analyzed, and quantified the economic impact of the COVID-19 pandemic and other events in critical markets such as the leveraged lending market.",
+    // "Investigated and wrote reports on specific issues requested by the Governors.",
+    // "Generated high-quality and novel academic research in macroeconomics, empirical banking, household finance, and financial institutions using a large confidential dataset",
     // ],
   },
 
@@ -64,8 +64,8 @@ const workExperienceTimelineData = [
     location: "CAGLIARI, ITALY",
     position: "Visiting Researcher",
     // data: [
-      // "Analyzed the impact of the introduction of the Single Supervisory Mechanism (SSM) on the credit supply from Italian banks and the implications for the Italian economy.",
-      // "Worked with a large proprietary and confidential dataset on the universe of loans and banks’ financial conditions to study the role of bank supervision in preventing credit rationing by financial institutions.",
+    // "Analyzed the impact of the introduction of the Single Supervisory Mechanism (SSM) on the credit supply from Italian banks and the implications for the Italian economy.",
+    // "Worked with a large proprietary and confidential dataset on the universe of loans and banks’ financial conditions to study the role of bank supervision in preventing credit rationing by financial institutions.",
     // ],
   },
   {
@@ -75,8 +75,8 @@ const workExperienceTimelineData = [
     location: "CHICAGO, ILLINOIS, US",
     position: "Visiting Scholar",
     // data: [
-      // "Conducted research in corporate governance, banking, and political economy.",
-      // "Analyzed data and write-up memos to provide key insights on the role of the CEO in spurring corporate innovation.",
+    // "Conducted research in corporate governance, banking, and political economy.",
+    // "Analyzed data and write-up memos to provide key insights on the role of the CEO in spurring corporate innovation.",
     // ],
   },
 
@@ -87,7 +87,7 @@ const workExperienceTimelineData = [
     location: "TRENTO, ITALY",
     position: "Intern",
     // data: [
-      // "Conducted quantitative and qualitative analysis on the impact of the Small Business Act (SBA) on the local Italian economy, which resulted in the publication of a report (here).",
+    // "Conducted quantitative and qualitative analysis on the impact of the Small Business Act (SBA) on the local Italian economy, which resulted in the publication of a report (here).",
     // ],
   },
 ]
