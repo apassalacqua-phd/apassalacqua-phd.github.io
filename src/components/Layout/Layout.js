e62d4a5468d1b14deacd98e6dcb4e47b17a697cf
XaFaK-01 <sanzarfarooq01@gmail.com>
2024-08-26 09:24:10 +0500

    optimized footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,26 +2,24 @@ import * as React from "react"
 
 import Header from "../Header/Header"
 
+import * as styles from "./Layout.module.css"
+
 const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <Header />
-      <main>{children}</main>
-      <footer
-        style={{
-          fontWeight: 700,
-          fontFamily: `"EB Garamond", Garamond, serif`,
-          fontSize: "22px",
-          padding: "10px",
-          margin: "0",
-        }}
-      >
-        Disclaimer: The opinions expressed on this website are solely those of
-        Andrea Passalacqua and do not necessarily reflect the views or opinions
-        of his employer.
+    <div style={{ minHeight: "100vh", position: "relative" }}>
+      <div className="global-wrapper" data-is-root-path={isRootPath}>
+        <Header />
+        <main>{children}</main>
+      </div>
+      <footer className={styles.footer}>
+        <em>
+          <strong> Disclaimer: </strong>The opinions expressed on this website
+          are solely those of Andrea Passalacqua and do not necessarily reflect
+          the views or opinions of his employer.
+        </em>
       </footer>
     </div>
   )
