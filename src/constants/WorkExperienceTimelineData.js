1d47dcffd0ee4d0228841ba233c72d239c5de8a6
apassalacqua-phd <Andrea.passalacqua@outlook.com>
2024-09-08 13:05:05 -0400

    refactored work experience data formatting

--- src/constants/WorkExperienceTimelineData.js
+++ src/constants/WorkExperienceTimelineData.js
@@ -4,11 +4,8 @@ const workExperienceTimelineData = [
     isPresent: true,
     entityName: "JPMORGAN CHASE & CO.",
     location: "Washington, DC, US",
-    position: "Vice President & Research Lead",
-    // data: [
-    // "Lead and prioritize impactful research projects related to small businesses and local economies, managing and triaging inbound requests for analysis to ensure timely completion and relevance.",
-    // "Conduct original analyses of large datasets using expert coding practices and big data programming languages on a public cloud platform, producing comprehensive statistical and visual analytics.",
-    // ],
+    data: ["Vice President & Research Lead"],
+   
   },
 
   {
@@ -16,12 +13,9 @@ const workExperienceTimelineData = [
     isPresent: true,
     entityName: "GEORGETOWN UNIVERSITY",
     location: "Washington, DC, US",
-    position:
-      "Visiting Research Fellow at the AI, Analytics, and the Future of Work Initiative",
-    // data: [
-    // "Lead and prioritize impactful research projects related to small businesses and local economies, managing and triaging inbound requests for analysis to ensure timely completion and relevance.",
-    // "Conduct original analyses of large datasets using expert coding practices and big data programming languages on a public cloud platform, producing comprehensive statistical and visual analytics.",
-    // ],
+    data:
+      ["Visiting Research Fellow at the AI, Analytics, and the Future of Work Initiative"],
+   
   },
 
   {
@@ -29,7 +23,7 @@ const workExperienceTimelineData = [
     isPresent: true,
     entityName: "JOHN HOPKINS CAREY BUSINESS SCHOOL",
     location: "Washington, DC, US",
-    position: "Adjunct Lecturer",
+    data: ["Adjunct Lecturer"],
   },
 
   {
@@ -38,7 +32,7 @@ const workExperienceTimelineData = [
     isPresent: false,
     entityName: "ANALYSIS GROUP",
     location: "Washington, DC, US",
-    position: "Associate",
+    data: ["Associate"],
   },
 
   {
@@ -47,13 +41,8 @@ const workExperienceTimelineData = [
     isPresent: false,
     entityName: "BOARD OF THE GOVERNORS OF THE FEDERAL RESERVE SYSTEM",
     location: "Washington, DC, US",
-    position: "Economist",
-    // data: [
-    // "Researched, designed, and implemented models to forecast banks’ potential losses during different hypothetical scenarios in the Dodd-Frank Act Stress Tests (DFAST).",
-    // "Monitored, analyzed, and quantified the economic impact of the COVID-19 pandemic and other events in critical markets such as the leveraged lending market.",
-    // "Investigated and wrote reports on specific issues requested by the Governors.",
-    // "Generated high-quality and novel academic research in macroeconomics, empirical banking, household finance, and financial institutions using a large confidential dataset",
-    // ],
+    data: ["Economist"],
+  
   },
 
   {
@@ -62,22 +51,16 @@ const workExperienceTimelineData = [
     isPresent: false,
     entityName: "BANK OF ITALY",
     location: "Cagliari, Italy",
-    position: "Visiting Researcher",
-    // data: [
-    // "Analyzed the impact of the introduction of the Single Supervisory Mechanism (SSM) on the credit supply from Italian banks and the implications for the Italian economy.",
-    // "Worked with a large proprietary and confidential dataset on the universe of loans and banks’ financial conditions to study the role of bank supervision in preventing credit rationing by financial institutions.",
-    // ],
+    data: ["Visiting Researcher"],
+   
   },
   {
     startYear: 2013,
     isPresent: false,
     entityName: "THE UNIVERSITY OF CHICAGO, BOOTH SCHOOL OF BUSINESS",
     location: "Chicago, Illinois, US",
-    position: "Visiting Scholar",
-    // data: [
-    // "Conducted research in corporate governance, banking, and political economy.",
-    // "Analyzed data and write-up memos to provide key insights on the role of the CEO in spurring corporate innovation.",
-    // ],
+    data: ["Visiting Scholar"],
+   
   },
 
   {
@@ -86,11 +69,8 @@ const workExperienceTimelineData = [
     isPresent: false,
     entityName: "UCLA Anderson School of Management",
     location: "Milan, Italy",
-    position: "External Research Assistant",
-    // data: [
-    // "Conducted research in corporate governance, banking, and political economy.",
-    // "Analyzed data and write-up memos to provide key insights on the role of the CEO in spurring corporate innovation.",
-    // ],
+    data: ["External Research Assistant"],
+  
   },
 
 
@@ -100,10 +80,8 @@ const workExperienceTimelineData = [
     isPresent: false,
     entityName: "IGIER - Innocenzo Gasparini Institute for Economic Research",
     location: "Milan, Italy",
-    position: "Research Assistant",
-    // data: [
-    // "Conducted quantitative and qualitative analysis on the impact of the Small Business Act (SBA) on the local Italian economy, which resulted in the publication of a report (here).",
-    // ],
+    data: ["Research Assistant"],
+  
   },
 
   {
@@ -111,10 +89,8 @@ const workExperienceTimelineData = [
     isPresent: false,
     entityName: "OECD, CENTER FOR ENTREPRENEURSHIP, SME, REGIONS, AND CITIES",
     location: "Trento, Italy",
-    position: "Policy Intern",
-    // data: [
-    // "Conducted quantitative and qualitative analysis on the impact of the Small Business Act (SBA) on the local Italian economy, which resulted in the publication of a report (here).",
-    // ],
+    data: ["Policy Intern"],
+ 
   },
 ]
 
