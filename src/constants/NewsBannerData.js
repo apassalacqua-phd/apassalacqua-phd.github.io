6e0e7eaa1c94bdcf809858f9469ecbc5bf33daf9
XaFaK-01 <sanzarfarooq01@gmail.com>
2024-09-15 21:40:22 +0500

    temp nulled newsBannerContent

--- src/constants/NewsBannerData.js
+++ src/constants/NewsBannerData.js
@@ -1,4 +1,4 @@
-const newsBannerContent =
-  "I will be going on a vacation soon and am looking forward to taking a break from my usual routine."
+const newsBannerContent = null
+// "I will be going on a vacation soon and am looking forward to taking a break from my usual routine."
 
 export default newsBannerContent
